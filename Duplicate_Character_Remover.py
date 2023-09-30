b12f8e64d08d7ed682e39ed5f0bc031d1cd85a98
unknown <arixxdelera21@gmail.com>
2023-09-30 22:01:22 +0800

    new file has been created

--- Duplicate_Character_Remover.py
+++ Duplicate_Character_Remover.py
@@ -1,11 +1,6 @@
 # Delera, Aritz B.
+
 # Create a CharacterRemovalProcess class
 # define the removal of duplicates
 # use for loop to add and append the characters
 # use join method to return the result
-
-# in seperate file, call the CharacterRemovalProcess class by importing the other file
-# interact with the user by asking the number of strings
-# declare the results to store the list
-# use for loop and call the method from the another file
-# display the results
---
+++ main.py
@@ -0,0 +1,7 @@
+# Delera, Aritz B.
+
+# in seperate file, call the CharacterRemovalProcess class by importing the other file
+# interact with the user by asking the number of strings
+# declare the results to store the list
+# use for loop and call the method from the another file
+# display the results
