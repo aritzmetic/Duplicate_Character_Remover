926c5ee6156a4820eaab888b2078056d7970f8d9
unknown <arixxdelera21@gmail.com>
2023-09-30 22:25:28 +0800

    code has been supplemented

--- Duplicate_Character_Remover.py
+++ Duplicate_Character_Remover.py
@@ -1,6 +1,20 @@
 # Delera, Aritz B.
 
 # Create a CharacterRemovalProcess class
-# define the removal of duplicates
-# use for loop to add and append the characters
-# use join method to return the result
+class CharacterRemovalProcess:
+    def __init__(self, user_input):
+        self.user_input = user_input
+
+    # define the removal of duplicates
+    def duplicates_removal(self):
+        present = set()
+        result = []
+
+        # use for loop to add and append the characters
+        for character in self.user_input:
+            if character not in present:
+                present.add(character)
+                result.append(character)
+
+        # use join method to return the result
+        return ''.join(result)
