a58775775e3f981ed1bcac061a24dd0171727f34
unknown <arixxdelera21@gmail.com>
2023-09-30 22:45:32 +0800

    code has been supplemented for the main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 # Delera, Aritz B.
 
 # in seperate file, call the CharacterRemovalProcess class by importing the other file
+from Duplicate_Character_Remover import CharacterRemovalProcess
+
 # interact with the user by asking the number of strings
-# declare the results to store the list
-# use for loop and call the method from the another file
+if __name__ == "__main__":
+    N = int(input("Enter the number of strings: "))
+
+    # declare the results to store the list
+    results = []
+
+    # use for loop and call the method from the another file
+    for s in range(N):
+        user_input = input("Enter a string: ")
+        remover = CharacterRemovalProcess(user_input)
+        duplicate_removed = remover.duplicates_removal()
+        results.append(duplicate_removed)
+
 # display the results
+    print("\nResults:")
+    for result in results:
+        print(result)
